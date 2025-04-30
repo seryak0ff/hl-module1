ac94ba0857a4d2625e343504dcf1f2717da294b8
seryak0ff <seryakoffserg@yandex.ru>
2025-04-30 16:10:52 +0300

    lab-12 working add dop500

--- src/main/java/ru/hpclab/hl/module1/kafkamodule/listener/KafkaMessageListener.java
+++ src/main/java/ru/hpclab/hl/module1/kafkamodule/listener/KafkaMessageListener.java
@@ -5,11 +5,17 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+
 import org.springframework.stereotype.Component;
 import ru.hpclab.hl.module1.kafkamodule.queue.KafkaOperationMessage;
 import ru.hpclab.hl.module1.kafkamodule.dispatch.KafkaMessageDispatcher;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @RequiredArgsConstructor
@@ -19,21 +25,45 @@ public class KafkaMessageListener {
     private final KafkaMessageDispatcher kafkaMessageDispatcher;
     private static final Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
 
+    private final AtomicInteger messageCounter = new AtomicInteger(0);
+
     @KafkaListener(
             topics = "${kafka.topic:articles-users-downloads}",
             groupId = "${kafka.groupId:seryak0ff-consumer-group}",
             concurrency = "${kafka.concurrency:2}", // Можно настроить по количеству партиций
             containerFactory = "kafkaListenerContainerFactory"
     )
-    public void handleMessage(List<String> messageJsonList) {
-        for (String messageJson : messageJsonList) {
+
+    public void handleMessage(@Payload List<String> messageJsonList, @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
+        for (int i = 0; i < messageJsonList.size(); i++) {
+            long offset = offsets.get(i);
+            String message = messageJsonList.get(i);
             try {
-                KafkaOperationMessage message = objectMapper.readValue(messageJson, KafkaOperationMessage.class);
-//                log.info("Received Kafka message: {}", message);
-                kafkaMessageDispatcher.dispatch(message);
+                KafkaOperationMessage messageKafka = objectMapper.readValue(message, KafkaOperationMessage.class);
+
+                if (offset % 500 == 0) {
+                    log.info("500th message reached! Count: {}, Message: {}",
+                            offset, message);
+                }
+                kafkaMessageDispatcher.dispatch(messageKafka);
             } catch (Exception e) {
-                log.error("Error while parsing or dispatching Kafka message: {}", messageJson, e);
+                log.error("Error while parsing or dispatching Kafka message: {}", message, e);
             }
         }
     }
+
+
+
+
+//    public void handleMessage(List<String> messageJsonList) {
+//        for (String messageJson : messageJsonList) {
+//            try {
+//                KafkaOperationMessage message = objectMapper.readValue(messageJson, KafkaOperationMessage.class);
+////                log.info("Received Kafka message: {}", message);
+//                kafkaMessageDispatcher.dispatch(message);
+//            } catch (Exception e) {
+//                log.error("Error while parsing or dispatching Kafka message: {}", messageJson, e);
+//            }
+//        }
+//    }
 }
